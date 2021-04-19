9886fe5d45406387a9591c54080785edd0b87a42
Omar Ali <omar199590@gmail.com>
2021-04-20 01:47:21 +0200

    deploy2

--- dataformtest/settings.py
+++ dataformtest/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cleanup.apps.CleanupConfig',
     'clear_cache',
     'reset_migrations',
+    'django_cleanup.apps.CleanupConfig'
 ]
 
 MIDDLEWARE = [

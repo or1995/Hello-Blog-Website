f08ce553e91bac69d0b2188136ed2a1344f11bca
Omar Ali <omar199590@gmail.com>
2021-04-21 01:11:32 +0200

    deploy 4

--- dataformtest/settings.py
+++ dataformtest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z1o&m60@4x+7arx^q45tsr#jwgg9&q19mb0hi*uaa=%ms-!2w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

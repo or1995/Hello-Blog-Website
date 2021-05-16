e03ecd172942a6804defa7427f36461f014cde5a
Omar Ali <omar199590@gmail.com>
2021-05-16 08:27:52 +0200

    database changes

--- dataformtest/settings.py
+++ dataformtest/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'dataformtest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'hellodatabase',
         'USER': 'postgres',
         'PASSWORD': 'omar1995',
-        'HOST': 'hello.cfyxlskb5ame.us-east-2.rds.amazonaws.com',
+        'HOST': 'hellodatabase.cfyxlskb5ame.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
